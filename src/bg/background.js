45f2afc8f8ec487b17518f72d49889decdc168fc
Taylor McLaughlin <44411229+tmclau96@users.noreply.github.com>
2019-03-22 18:12:45 -0400

    Update background.js

--- src/bg/background.js
+++ src/bg/background.js
@@ -8,6 +8,14 @@
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
+  	//chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    return {redirectUrl: chrome.runtime.getURL("src/browser_action/warning_page.html")};
+  },
+  {urls: ["https://www.google.com/"], types: ["main_frame", "sub_frame"]},  //redirects users when going to google for testing purposes only
+  ["blocking"]
+);
