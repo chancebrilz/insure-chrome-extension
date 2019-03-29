928bc312e54b60411c6fb473e1662f49ff3d74dd
chancebrilz <chancebrilz@gmail.com>
2019-03-29 11:33:35 -0400

    Testing tab loading

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,21 +1,21 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	//chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+  //chrome.pageAction.show(sender.tab.id);
+  sendResponse();
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, info) {
+  if (info.status === "complete") {
+    console.log("ThirdEye:", "Tab Loaded", tabId, info);
+  }
+});
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-    return {redirectUrl: chrome.runtime.getURL("src/browser_action/warning_page.html")};
+    return {
+      redirectUrl: chrome.runtime.getURL("src/browser_action/warning_page.html")
+    };
   },
-  {urls: ["https://www.google.com/"], types: ["main_frame", "sub_frame"]},  //redirects users when going to google for testing purposes only
+  { urls: ["https://www.google.com/"], types: ["main_frame", "sub_frame"] }, //redirects users when going to google for testing purposes only
   ["blocking"]
 );
