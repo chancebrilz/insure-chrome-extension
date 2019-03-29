170bbafd6bf9785ffb741efc79efeb34dab65e66
chancebrilz <chancebrilz@gmail.com>
2019-03-29 11:47:50 -0400

    Redirect to warning on google

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,21 +1,35 @@
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-  //chrome.pageAction.show(sender.tab.id);
-  sendResponse();
-});
+// chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+//   //chrome.pageAction.show(sender.tab.id);
+//   sendResponse();
+// });
+
+// chrome.webRequest.onBeforeRequest.addListener(
+//   function(details) {
+//     return {
+//       redirectUrl: chrome.runtime.getURL("src/browser_action/warning_page.html")
+//     };
+//   },
+//   { urls: ["https://www.google.com/"], types: ["main_frame", "sub_frame"] }, //redirects users when going to google for testing purposes only
+//   ["blocking"]
+// );
 
 chrome.tabs.onUpdated.addListener(function(tabId, info) {
   if (info.status === "complete") {
-    console.log("ThirdEye:", "Tab Loaded", tabId, info);
+    chrome.tabs.get(tabId, function(tab) {
+      const { url } = tab;
+
+      // HERE WE CAN CHECK FOR A VALID/INVALID URL
+      console.log("Checking url", url, "...");
+
+      if (url === "https://www.google.com/") {
+        chrome.tabs.create(
+          {
+            url: "/src/browser_action/warning_page.html"
+          },
+          function() {}
+        );
+      }
+    });
   }
 });
-
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    return {
-      redirectUrl: chrome.runtime.getURL("src/browser_action/warning_page.html")
-    };
-  },
-  { urls: ["https://www.google.com/"], types: ["main_frame", "sub_frame"] }, //redirects users when going to google for testing purposes only
-  ["blocking"]
-);
