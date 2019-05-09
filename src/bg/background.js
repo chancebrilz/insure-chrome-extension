ab700cc27b4414596e613eb70f3d998dd684db85
chancebrilz <chancebrilz@gmail.com>
2019-05-09 18:58:03 -0400

    pass full URL to api as well

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,3 +1,5 @@
+const DISABLED_KEY = "THIRD_EYE_DISABLED";
+
 class API {
   apiHost = "http://cosc490.chance.sh/";
 
@@ -17,7 +19,7 @@ class API {
     const host = this.getHost(lookupUrl);
 
     if (host !== null) {
-      const url = `${this.apiHost}?url=${host}`;
+      const url = `${this.apiHost}?url=${host}&fullUrl=${lookupUrl}`;
 
       return fetch(url).then(response => response.json());
     }
@@ -27,54 +29,56 @@ class API {
 }
 
 chrome.tabs.onUpdated.addListener((tabId, info) => {
-  chrome.tabs.get(tabId, async tab => {
-    const { url } = tab;
+  const enabled = localStorage.getItem(DISABLED_KEY) != "true";
 
-    var site = url;
-    chrome.runtime.sendMessage({ site });
+  if (enabled) {
+    chrome.tabs.get(tabId, async tab => {
+      const { url } = tab;
 
-    if (url) {
-      // Check if the URL is the whitelist or blacklist
+      var site = url;
+      chrome.runtime.sendMessage({ site });
 
-      const api = new API();
-      const host = api.getHost(url);
+      if (url) {
+        // Check if the URL is the whitelist or blacklist
 
-      //Fetch and parse whitelist and blacklist from local storage
-      var wl_arr = JSON.parse(localStorage.getItem("whitelist"));
-      var bl_arr = JSON.parse(localStorage.getItem("blacklist"));
-      //console.log(bl_arr);
-      console.log(host);
-      if (host !== null) {
-        if (wl_arr !== null) {
-          //Search whitelist for matching url
-          for (var i = 0; i < wl_arr.length; i++) {
-            if (wl_arr[i] == host || host == "www." + wl_arr[i]) {
-              //URL is in whitelist, so disregard
-              return;
+        const api = new API();
+        const host = api.getHost(url);
+
+        //Fetch and parse whitelist and blacklist from local storage
+        var wl_arr = JSON.parse(localStorage.getItem("whitelist"));
+        var bl_arr = JSON.parse(localStorage.getItem("blacklist"));
+
+        if (host !== null) {
+          if (wl_arr !== null) {
+            //Search whitelist for matching url
+            for (var i = 0; i < wl_arr.length; i++) {
+              if (wl_arr[i] == host || host == "www." + wl_arr[i]) {
+                //URL is in whitelist, so disregard
+                return;
+              }
+            }
+          }
+          if (bl_arr !== null) {
+            //Search blacklist for matching url
+            for (var j = 0; j < bl_arr.length; j++) {
+              if (bl_arr[j] == host || host == "www." + bl_arr[j]) {
+                //Redirect to warning page
+                chrome.tabs.update(tabId, {
+                  url: "/src/browser_action/warning_page.html"
+                });
+              }
             }
           }
         }
-        if (bl_arr !== null) {
-          //Search blacklist for matching url
-          for (var j = 0; j < bl_arr.length; j++) {
-            if (bl_arr[j] == host || host == "www." + bl_arr[j]) {
-              //Redirect to warning page
-              chrome.tabs.update(tabId, {
-                url: "/src/browser_action/warning_page.html"
-              });
-            }
-          }
+
+        const response = await api.lookup(url);
+
+        if (response && response.malicious === true) {
+          chrome.tabs.update(tabId, {
+            url: `/src/browser_action/warning_page.html?url=${url}`
+          });
         }
       }
-
-      const response = await api.lookup(url);
-
-      if (response && response.malicious === true) {
-        // [1]
-        chrome.tabs.update(tabId, {
-          url: `/src/browser_action/warning_page.html?url=${url}`
-        });
-      }
-    }
-  });
+    });
+  }
 });
